3ea2a20d3e1680acc6c4772dd19776b06c79f43c
Ivan Romanov <3gopobo@gmail.com>
2024-07-08 10:35:05 +0000

    added emailJs function

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,17 +1,21 @@
+console.log("HEllo")
 
 function sendMail(contactForm) {
     emailjs.send("beauty","template_beauty", {
-        "from_name": contactForm.inputName.value,
-        "last_name": contactForm.inputLastName.value,
-        "phone_number": contactForm.inputPhone.value,
-        "from_email": contactForm.inputEmail.value,
-        "procedure": contactForm.inputProcedure.value
+        from_name: contactForm.inputName.value,
+        last_name: contactForm.inputLastName.value,
+        phone_number: contactForm.inputPhone.value,
+        from_email: contactForm.inputEmail.value,
+        procedure: contactForm.inputProcedure.value,
+        
     })
     .then(
         function(response) {
             console.log("SUCCES", response);
         },
-      function(error) {
+        function(error) {
         console.log("FAILED", error);
-      });
+        }
+    );
+        return false;
 }
